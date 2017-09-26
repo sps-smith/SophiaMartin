5f72d408127cc3c0ff1ee9171fb1386c022dddc8
Phil Smith <phil.smith@volarisgroup.com>
2017-09-26 16:52:09 -0400

    Initial Load

---
+++ website/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Navigation></Navigation>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation.vue';
+
+export default {
+  name: 'app',
+  components:{
+    'Navigation': Navigation
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ website/src/Navigation.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">            
+                <div class="navbar-header">                
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse">               
+                    <ul class="nav navbar-nav">
+                        <li> <router-link :to="`/Home`">Home</router-link></li>
+                        <li> <router-link :to="`/About`">About</router-link></li>
+                        <li> <router-link :to="`/Contact`">Contact</router-link></li>
+                        <li> <router-link :to="`/Books`">Books</router-link></li>
+                        <li> <router-link :to="`/Sermons`">Sermons</router-link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="container" style="margin-top:5px;">
+            <div class="row">
+                <div class="col-md-12">
+                    <div style="float:left;">
+                        <img src="./images/img_1284.jpg" class="logo img-responsive img-thumbnail" />
+                    </div>
+                    <div class="namecss">
+                        <h1 class="stitle">Rev. Sophia Martin</h1>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Navigation'
+    }
+</script>
+
+<style>
+.navbar-inverse {
+    background-color: #d48c06;
+    border: none;
+    margin-bottom: 5px;
+}
+.navbar-inverse .navbar-nav > li > a {
+    color: #000;
+}
+.logo {
+    max-width: 180px;
+}
+.namecss
+{
+    max-width: 600px;
+    padding-top: 20px;
+}
+</style>
